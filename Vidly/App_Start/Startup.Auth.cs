15fa77cb04f18e6d9b91630bbdc22651e5b336bf
m.sohrabi1120 <m.sohrabi1120@gmail.com>
2022-12-25 19:47:06 +0330

    test

--- Vidly/App_Start/Startup.Auth.cs
+++ Vidly/App_Start/Startup.Auth.cs
@@ -14,7 +14,7 @@ namespace Vidly
         // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
+            // Configure the db context, user manager and signin manager to use a single instance per requesttttt
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);

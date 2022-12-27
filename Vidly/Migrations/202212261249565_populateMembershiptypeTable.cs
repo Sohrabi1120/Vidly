37c4b060ff474e5f63bc6672b80944287810c85f
m.sohrabi1120 <m.sohrabi1120@gmail.com>
2022-12-27 09:59:32 +0330

    Name property added to membership type and customerview edited.

--- Vidly/Migrations/202212261249565_populateMembershiptypeTable.cs
+++ Vidly/Migrations/202212261249565_populateMembershiptypeTable.cs
@@ -7,10 +7,10 @@
     {
         public override void Up()
         {
-            Sql("Insert into Membershiptypes(Id, SighUpFee, DurationInMonths, DiscountRate) Values( 1, 0, 0, 0)");
-            Sql("Insert into Membershiptypes(Id, SighUpFee, DurationInMonths, DiscountRate) Values( 2, 30, 1, 10)");
-            Sql("Insert into Membershiptypes(Id, SighUpFee, DurationInMonths, DiscountRate) Values( 3, 90, 3, 15)");
-            Sql("Insert into Membershiptypes(Id, SighUpFee, DurationInMonths, DiscountRate) Values( 4, 300, 12, 20)");
+            Sql("Insert into Membershiptypes(Id, SignUpFee, DurationInMonths, DiscountRate) Values( 1, 0, 0, 0)");
+            Sql("Insert into Membershiptypes(Id, SignUpFee, DurationInMonths, DiscountRate) Values( 2, 30, 1, 10)");
+            Sql("Insert into Membershiptypes(Id, SignUpFee, DurationInMonths, DiscountRate) Values( 3, 90, 3, 15)");
+            Sql("Insert into Membershiptypes(Id, SignUpFee, DurationInMonths, DiscountRate) Values( 4, 300, 12, 20)");
         }
         
         public override void Down()
---
+++ Vidly/Migrations/202212270613176_addNameToMembershipType.cs
@@ -0,0 +1,22 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addNameToMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String());
+            Sql("update membershiptypes set Name=N'Pay as You Go' where Id = 1");
+            Sql("update membershiptypes set Name=N'Monthly' where Id = 2");
+            Sql("update membershiptypes set Name=N'Quarterly' where Id = 3");
+            Sql("update membershiptypes set Name=N'Anualy' where Id = 4");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
